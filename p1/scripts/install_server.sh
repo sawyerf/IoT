c5114b4e2cfa5c4cc2f22ca24422f32bf8cf1791
Sawyerf <sawyer.flink@protonmail.ch>
2022-06-02 14:18:06 +0200

    Fix: installation

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd /etc/yum.repos.d/
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-yum update -y
-cd -
+# cd /etc/yum.repos.d/
+# sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+# sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+# yum update -y
+# cd -
 curl -sfL https://get.k3s.io | sh -
--- p1/scripts/install_serverWorker.sh
+++ p1/scripts/install_serverWorker.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-cd /etc/yum.repos.d/
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+# cd /etc/yum.repos.d/
+# sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+# sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 # yum update -y
-cd -
+# cd -
 
+# Token is in `sudo cat /var/lib/rancher/k3s/server/node-token`
 curl -sfL https://get.k3s.io | K3S_URL=https://192.168.42.110:6443 K3S_TOKEN=mynodetoken sh -
