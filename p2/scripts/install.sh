9e1dd511bcefa35c95c10d799b2af689a2b8501f
vsaltel <saltel.valentin@gmail.com>
2022-06-07 15:54:44 +0200

    p2 fonctionnel

---
+++ p2/scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode K3S_KUBECONFIG_MODE=644 --flannel-iface=eth1
+echo "alias k='sudo /usr/local/bin/kubectl'" >> /home/vagrant/.bashrc
+sudo /usr/local/bin/kubectl create configmap app1 --from-file /vagrant/confs/app1/index.html
+sudo /usr/local/bin/kubectl apply -f /vagrant/confs/app1/app1.yml
+sudo /usr/local/bin/kubectl create configmap app2 --from-file /vagrant/confs/app2/index.html
+sudo /usr/local/bin/kubectl apply -f /vagrant/confs/app2/app2.yml
+sudo /usr/local/bin/kubectl create configmap app3 --from-file /vagrant/confs/app3/index.html
+sudo /usr/local/bin/kubectl apply -f /vagrant/confs/app3/app3.yml
