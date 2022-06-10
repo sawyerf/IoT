8c4b54ff798866eab34997ae2ec53386fe277243
vsaltel <saltel.valentin@gmail.com>
2022-06-10 17:10:52 +0200

    deployment working

--- p3/scripts/set_config.sh
+++ p3/scripts/set_config.sh
@@ -5,12 +5,12 @@ echo set_config
 sudo kubectl create namespace argocd
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.3.4/manifests/install.yaml
 sudo kubectl wait pods --all -n argocd --for condition=Ready --timeout=90s
-sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 --address 0.0.0.0 &
+sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 --address 0.0.0.0 1>&- &
 sleep 1
 sudo kubectl create namespace dev
-argocd app create app-v1 --repo https://github.com/sawyerf/tmp-iot --path v1 --dest-server https://kubernetes.default.svc --dest-namespace dev
-argocd app get app-v1
-argocd app sync app-v1
+sudo argocd login localhost:8080 --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo) --insecure
+sudo argocd app create app-v1 --repo https://github.com/sawyerf/tmp-iot --path playground --dest-server https://kubernetes.default.svc --dest-namespace dev
+sudo argocd app sync app-v1
 
 #sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 #sudo argocd login localhost:8080
