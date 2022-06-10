b0a891464bbcfb7237319d8930be88b14fc7c688
vsaltel <saltel.valentin@gmail.com>
2022-06-10 14:21:12 +0200

    deployment test

--- p3/scripts/install_requirement.sh
+++ p3/scripts/install_requirement.sh
@@ -2,30 +2,30 @@
 
 echo install_requirement
 
-sudo apt-get update
-# Install git
-sudo apt -y install git
-# Install k3s
-sudo curl -sfL https://get.k3s.io | sh -
-# Install k3d
-sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-# Install docker
-sudo apt-get -y install \
+apt-get -y update
+echo Install ssh
+apt install -y ssh
+echo Install git
+apt install -y git
+echo Install curl
+apt install -y curl
+echo Install k3s
+curl -sfL https://get.k3s.io | sh -
+echo Install k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+echo Install docker
+apt-get -y install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-if [[ $? -eq 0 ]]
-then
-	sudo mkdir -p /etc/apt/keyrings
-	sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-	sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt-get update
-	sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-fi
-# Install ArgoCD
-sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-if [[ $? -eq 0 ]]
-then
-	sudo chmod +x /usr/local/bin/argocd
-fi
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get -y update
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo service docker start
+echo Install ArgoCD
+curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+chmod +x /usr/local/bin/argocd
+echo "alias k='sudo /usr/local/bin/kubectl'" >> /home/user/.bashrc
