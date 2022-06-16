778eb3e9e3bafc2c23b7949ace8c4a5a0cfa983d
Sawyerf <sawyer.flink@protonmail.ch>
2022-06-16 14:23:48 +0200

    Add: vagrantfile

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Server Worker
-curl -sfL https://get.k3s.io | sh -s - server --flannel-iface=enp0s3
+curl -sfL https://get.k3s.io | sh -s - server --flannel-iface=eth1
 # Install docker
 apt update
 yes | apt install \
@@ -16,21 +16,5 @@ apt update
 yes | apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 service docker start
 # Run Gitlab 
-kubectl apply -f deployment.yml
+kubectl apply -f /vagrant/confs/deployment.yml
 kubectl exec deployment/gitlab -- grep 'Password:' /etc/gitlab/initial_root_password
-
-
-# Install gitlab
-# export GITLAB_HOME=/srv/gitlab
-# mkdir -p $GITLAB_HOME
-# sudo docker run --detach \
-#   --hostname 192.168.1.197 \
-#   --publish 443:443 --publish 80:80 \
-#   --name gitlab \
-#   --restart always \
-#   --volume $GITLAB_HOME/config:/etc/gitlab \
-#   --volume $GITLAB_HOME/logs:/var/log/gitlab \
-#   --volume $GITLAB_HOME/data:/var/opt/gitlab \
-#   --shm-size 256m \
-#   gitlab/gitlab-ee:latest
-# sudo docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password
