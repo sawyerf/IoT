ef2afe05383554321846a58bfd9ab6a925ea2d6b
Sawyerf <sawyer.flink@protonmail.ch>
2022-06-17 14:17:27 +0200

    Up: bonus

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Server Worker
-curl -sfL https://get.k3s.io | sh -s - server --flannel-iface=eth1
-# Install docker
-sudo yum install -y yum-utils
-sudo yum-config-manager \
-   --add-repo \
-   https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo systemctl start docker
-# Run Gitlab 
-sudo /usr/local/bin/kubectl apply -f /vagrant/confs/deployment.yml
-# sudo /usr/local/bin/kubectl exec deployment/gitlab -- grep 'Password:' /etc/gitlab/initial_root_password
+echo '[*] Create namespace'
+sudo kubectl create namespace gitlab
+echo '[*] Run Gitlab'
+sudo /usr/local/bin/kubectl apply -f confs/deployment.yml -n gitlab
+echo '[*] Wait Gitlab pods'
+sudo kubectl wait pods --all -n gitlab --for condition=Ready
+echo '[*] Get password'
+sudo /usr/local/bin/kubectl exec deployment/gitlab -n gitlab -- grep 'Password:' /etc/gitlab/initial_root_password
