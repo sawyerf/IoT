9a52921ca2cb6ebffe47b0630444cefa5a6da417
LesDeuxZigotos <LesDeuxZigotos@desbarres.fr>
2022-06-20 12:39:16 -0400

    Up: bonus

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -5,6 +5,6 @@ sudo kubectl create namespace gitlab
 echo '[*] Run Gitlab'
 sudo /usr/local/bin/kubectl apply -f confs/deployment.yml -n gitlab
 echo '[*] Wait Gitlab pods'
-sudo kubectl wait pods --all -n gitlab --for condition=Ready
+sudo kubectl wait pods --all -n gitlab --for condition=Ready --timeout=300s
 echo '[*] Get password'
 sudo /usr/local/bin/kubectl exec deployment/gitlab -n gitlab -- grep 'Password:' /etc/gitlab/initial_root_password
